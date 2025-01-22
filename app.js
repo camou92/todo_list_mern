a5ed2fcfef9bc1f8a6ca424406dfc26e4977d751
camou92 <cmohamed992@gmail.com>
2025-01-23 00:29:18 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express"
+import dotenv from "dotenv";
+import connectDB from "./db/database.js";
+const app = express();
+dotenv.config()
+
+connectDB()
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server listen at port ${PORT}`);
+})
